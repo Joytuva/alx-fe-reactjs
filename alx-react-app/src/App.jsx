7c46aae40f7c4e7c4fa671b49da12a715f64e55a
Joy <tuvamutanu@gmail.com>
2025-08-26 21:29:45 +0300

    Made changes to files src/App.jsx src/Footer.jsx src/Header.jsx src/MainContent.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,13 @@
-// src/App.jsx
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
