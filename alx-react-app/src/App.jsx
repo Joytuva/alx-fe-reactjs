be58b4b137be477976d47e054bc2d5d705d2917e
Joy <tuvamutanu@gmail.com>
2025-08-27 23:41:53 +0300

    Modified file App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,15 @@
+// App.jsx
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <MainContent />
       <Footer />
-    </div>
+    </>
   );
 }
 
