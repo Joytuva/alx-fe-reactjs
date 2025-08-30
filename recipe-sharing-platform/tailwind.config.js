e66fcc56e2fde75a5485ddc3c290b9a4ff19d4c8
Joy <tuvamutanu@gmail.com>
2025-08-30 12:20:00 +0300

    Modified file tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html',           // Root HTML file
+    './index.html',            // Root HTML file (Vite puts index.html here)
     './src/**/*.{js,jsx,ts,tsx}' // All JS/JSX/TS/TSX files
   ],
   theme: {
